085cbb3f77a12f86806d52743641b4bf24d9250f
Vidhi1010 <vidhisinghvp1010@gmail.com>
2025-07-17 22:59:10 +0530

    #

--- frontend/lib/firebase.ts
+++ frontend/lib/firebase.ts
@@ -1,21 +1,17 @@
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your Firebase config
 const firebaseConfig = {
-    apiKey: "AIzaSyDE6xjKWukXbbAidKY4iQXqUzCFCgMURXI",
-    authDomain: "waitlist-6cf53.firebaseapp.com",
-    projectId: "waitlist-6cf53",
-    storageBucket: "waitlist-6cf53.appspot.com",
-    messagingSenderId: "969436626923",
-    appId: "1:969436626923:web:1c35ea9f20860db688a3ae"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Get Firestore instance
 const db = getFirestore(app);
 
 export { db };
